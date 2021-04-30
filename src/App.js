673904513a08da05951b8ffb34b393d8c909c9a3
thomasguillaux <thomasguillaux@gmail.com>
2021-04-30 14:46:58 +0200

    navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { createGlobalStyle } from "styled-components";
+import { Navbar } from "./Navbar/Navbar";
 
 const GlobalStyle = createGlobalStyle`
   body {
     font-family: 'Open Sans', sans-serif;
+    margin: 0;
   }
 
   h1, h2, h3 {
@@ -14,6 +16,7 @@ function App() {
     return (
         <>
             <GlobalStyle />
+            <Navbar />
             <div className="App">Hello Pizza</div>
         </>
     );
